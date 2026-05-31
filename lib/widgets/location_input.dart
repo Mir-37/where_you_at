4544c7006b51c1bfd9bc524f35582024f476315a
Mir-37 <mirmutasim37@gmail.com>
2026-05-31 23:17:54 +0600

    feat(ui): implement location retrieval in LocationInput
    - Add `location` dependency to `pubspec.yaml`
    - Implement `_getCurrentLocation` logic in `LocationInput` widget to handle permission requests and service enablement
    - Add necessary camera and location permissions to `AndroidManifest.xml`
    - Update macOS plugin registrant to include `location` plugin

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,6 +10,37 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    if (!kIsWeb) {
+      serviceEnabled = await location.serviceEnabled();
+      if (!serviceEnabled) {
+        serviceEnabled = await location.requestService();
+        if (!serviceEnabled) return;
+      }
+
+      permissionGranted = await location.hasPermission();
+      if (permissionGranted == PermissionStatus.denied) {
+        permissionGranted = await location.requestPermission();
+        if (permissionGranted != PermissionStatus.granted) return;
+      }
+    }
+
+    try {
+      locationData = await location.getLocation();
+      debugPrint(
+        'Location: ${locationData.latitude}, ${locationData.longitude}',
+      );
+    } catch (e) {
+      debugPrint('Location error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -34,7 +67,7 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               icon: const Icon(
                 Icons.location_on,
               ),
